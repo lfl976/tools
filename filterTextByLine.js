34f58c8426fb03774cbe12ed6329428f1c65f28b
liufulin <976529466@qq.com>
2023-08-18 23:54:41 +0900

    filterTextByLine

---
+++ filterTextByLine.js
@@ -0,0 +1,27 @@
+/**
+ * 从zsh的history历史中过滤安装的软件
+ * history命令导出历史：history > history.txt
+ */
+const fs = require("fs");
+
+// 读取文件的路径
+const filePath = "/Users/liufulin/Desktop/history20230814.txt";
+
+// 使用UTF-8编码读取文件内容
+fs.readFile(filePath, "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
+
+  // 将文件内容按行分割成数组
+  const lines = data.split("\n");
+
+  // 遍历每一行并输出
+  lines.forEach((line) => {
+    line = line.replace(/^[0-9\s]+/, "");
+    if (line.match(/^brew(?:\s)+install(?:\s)+--cask/)) {
+      console.log(line);
+    }
+  });
+});
