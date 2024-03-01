bf600d5f63cf04a3457d1eaf4aa5a93824479008
liufulin <976529466@qq.com>
2024-03-01 17:30:16 +0900

    update: 语音识别翻译

---
+++ language.js
@@ -0,0 +1,541 @@
+var languages = [
+  {
+    name: "南非荷兰语",
+    value: "af",
+  },
+  {
+    name: "阿尔巴尼亚语",
+    value: "sq",
+  },
+  {
+    name: "阿姆哈拉语",
+    value: "am",
+  },
+  {
+    name: "阿拉伯语",
+    value: "ar",
+  },
+  {
+    name: "亚美尼亚文",
+    value: "hy",
+  },
+  {
+    name: "阿萨姆语",
+    value: "as",
+  },
+  {
+    name: "艾马拉语",
+    value: "ay",
+  },
+  {
+    name: "阿塞拜疆语",
+    value: "az",
+  },
+  {
+    name: "班巴拉语",
+    value: "bm",
+  },
+  {
+    name: "巴斯克语",
+    value: "eu",
+  },
+  {
+    name: "白俄罗斯语",
+    value: "be",
+  },
+  {
+    name: "孟加拉文",
+    value: "bn",
+  },
+  {
+    name: "博杰普尔语",
+    value: "bho",
+  },
+  {
+    name: "波斯尼亚语",
+    value: "bs",
+  },
+  {
+    name: "保加利亚语",
+    value: "bg",
+  },
+  {
+    name: "加泰罗尼亚语",
+    value: "ca",
+  },
+  {
+    name: "宿务语",
+    value: "ceb",
+  },
+  {
+    name: "中文（简体）",
+    value: "zh-CN 或 zh (BCP-47)",
+    valueBk: "zh-CN 或 zh (BCP-47)",
+  },
+  {
+    name: "中文（繁体）",
+    value: "zh-TW (BCP-47)",
+    valueBk: "zh-TW (BCP-47)",
+  },
+  {
+    name: "科西嘉语",
+    value: "co",
+  },
+  {
+    name: "克罗地亚语",
+    value: "hr",
+  },
+  {
+    name: "捷克语",
+    value: "cs",
+  },
+  {
+    name: "丹麦语",
+    value: "da",
+  },
+  {
+    name: "迪维希语",
+    value: "dv",
+  },
+  {
+    name: "多格来语",
+    value: "doi",
+  },
+  {
+    name: "荷兰语",
+    value: "nl",
+  },
+  {
+    name: "英语",
+    value: "en",
+  },
+  {
+    name: "世界语",
+    value: "eo",
+  },
+  {
+    name: "爱沙尼亚语",
+    value: "et",
+  },
+  {
+    name: "埃维语",
+    value: "ee",
+  },
+  {
+    name: "菲律宾语（塔加拉语）",
+    value: "fil",
+  },
+  {
+    name: "芬兰语",
+    value: "fi",
+  },
+  {
+    name: "法语",
+    value: "fr",
+  },
+  {
+    name: "弗里斯兰语",
+    value: "fy",
+  },
+  {
+    name: "加利西亚语",
+    value: "gl",
+  },
+  {
+    name: "格鲁吉亚语",
+    value: "ka",
+  },
+  {
+    name: "德语",
+    value: "de",
+  },
+  {
+    name: "希腊文",
+    value: "el",
+  },
+  {
+    name: "瓜拉尼人",
+    value: "gn",
+  },
+  {
+    name: "古吉拉特文",
+    value: "gu",
+  },
+  {
+    name: "海地克里奥尔语",
+    value: "ht",
+  },
+  {
+    name: "豪萨语",
+    value: "ha",
+  },
+  {
+    name: "夏威夷语",
+    value: "haw",
+  },
+  {
+    name: "希伯来语",
+    value: "he 或 iw",
+  },
+  {
+    name: "印地语",
+    value: "hi",
+  },
+  {
+    name: "苗语",
+    value: "hmn",
+  },
+  {
+    name: "匈牙利语",
+    value: "hu",
+  },
+  {
+    name: "冰岛语",
+    value: "is",
+  },
+  {
+    name: "伊博语",
+    value: "ig",
+  },
+  {
+    name: "伊洛卡诺语",
+    value: "ilo",
+  },
+  {
+    name: "印度尼西亚语",
+    value: "id",
+  },
+  {
+    name: "爱尔兰语",
+    value: "ga",
+  },
+  {
+    name: "意大利语",
+    value: "it",
+  },
+  {
+    name: "日语",
+    value: "ja",
+  },
+  {
+    name: "爪哇语",
+    value: "jv 或 jw",
+    valueBk: "jv 或 jw",
+  },
+  {
+    name: "卡纳达文",
+    value: "kn",
+  },
+  {
+    name: "哈萨克语",
+    value: "kk",
+  },
+  {
+    name: "高棉语",
+    value: "km",
+  },
+  {
+    name: "卢旺达语",
+    value: "rw",
+  },
+  {
+    name: "贡根语",
+    value: "gom",
+  },
+  {
+    name: "韩语",
+    value: "ko",
+  },
+  {
+    name: "克里奥尔语",
+    value: "kri",
+  },
+  {
+    name: "库尔德语",
+    value: "ku",
+  },
+  {
+    name: "库尔德语（索拉尼）",
+    value: "ckb",
+  },
+  {
+    name: "吉尔吉斯语",
+    value: "ky",
+  },
+  {
+    name: "老挝语",
+    value: "lo",
+  },
+  {
+    name: "拉丁文",
+    value: "la",
+  },
+  {
+    name: "拉脱维亚语",
+    value: "lv",
+  },
+  {
+    name: "林格拉语",
+    value: "ln",
+  },
+  {
+    name: "立陶宛语",
+    value: "lt",
+  },
+  {
+    name: "卢干达语",
+    value: "lg",
+  },
+  {
+    name: "卢森堡语",
+    value: "lb",
+  },
+  {
+    name: "马其顿语",
+    value: "mk",
+  },
+  {
+    name: "迈蒂利语",
+    value: "mai",
+  },
+  {
+    name: "马尔加什语",
+    value: "mg",
+  },
+  {
+    name: "马来语",
+    value: "ms",
+  },
+  {
+    name: "马拉雅拉姆文",
+    value: "ml",
+  },
+  {
+    name: "马耳他语",
+    value: "mt",
+  },
+  {
+    name: "毛利语",
+    value: "mi",
+  },
+  {
+    name: "马拉地语",
+    value: "mr",
+  },
+  {
+    name: "梅泰语（曼尼普尔语）",
+    value: "mni-Mtei",
+  },
+  {
+    name: "米佐语",
+    value: "lus",
+  },
+  {
+    name: "蒙古文",
+    value: "mn",
+  },
+  {
+    name: "缅甸语",
+    value: "my",
+  },
+  {
+    name: "尼泊尔语",
+    value: "ne",
+  },
+  {
+    name: "挪威语",
+    value: "no",
+  },
+  {
+    name: "尼杨扎语（齐切瓦语）",
+    value: "ny",
+  },
+  {
+    name: "奥里亚语（奥里亚）",
+    value: "or",
+  },
+  {
+    name: "奥罗莫语",
+    value: "om",
+  },
+  {
+    name: "普什图语",
+    value: "ps",
+  },
+  {
+    name: "波斯语",
+    value: "fa",
+  },
+  {
+    name: "波兰语",
+    value: "pl",
+  },
+  {
+    name: "葡萄牙语（葡萄牙、巴西）",
+    value: "pt",
+  },
+  {
+    name: "旁遮普语",
+    value: "pa",
+  },
+  {
+    name: "克丘亚语",
+    value: "qu",
+  },
+  {
+    name: "罗马尼亚语",
+    value: "ro",
+  },
+  {
+    name: "俄语",
+    value: "ru",
+  },
+  {
+    name: "萨摩亚语",
+    value: "sm",
+  },
+  {
+    name: "梵语",
+    value: "sa",
+  },
+  {
+    name: "苏格兰盖尔语",
+    value: "gd",
+  },
+  {
+    name: "塞佩蒂语",
+    value: "nso",
+  },
+  {
+    name: "塞尔维亚语",
+    value: "sr",
+  },
+  {
+    name: "塞索托语",
+    value: "st",
+  },
+  {
+    name: "修纳语",
+    value: "sn",
+  },
+  {
+    name: "信德语",
+    value: "sd",
+  },
+  {
+    name: "僧伽罗语",
+    value: "si",
+  },
+  {
+    name: "斯洛伐克语",
+    value: "sk",
+  },
+  {
+    name: "斯洛文尼亚语",
+    value: "sl",
+  },
+  {
+    name: "索马里语",
+    value: "so",
+  },
+  {
+    name: "西班牙语",
+    value: "es",
+  },
+  {
+    name: "巽他语",
+    value: "su",
+  },
+  {
+    name: "斯瓦希里语",
+    value: "sw",
+  },
+  {
+    name: "瑞典语",
+    value: "sv",
+  },
+  {
+    name: "塔加路语（菲律宾语）",
+    value: "tl",
+  },
+  {
+    name: "塔吉克语",
+    value: "tg",
+  },
+  {
+    name: "泰米尔语",
+    value: "ta",
+  },
+  {
+    name: "鞑靼语",
+    value: "tt",
+  },
+  {
+    name: "泰卢固语",
+    value: "te",
+  },
+  {
+    name: "泰语",
+    value: "th",
+  },
+  {
+    name: "蒂格尼亚语",
+    value: "ti",
+  },
+  {
+    name: "宗加语",
+    value: "ts",
+  },
+  {
+    name: "土耳其语",
+    value: "tr",
+  },
+  {
+    name: "土库曼语",
+    value: "tk",
+  },
+  {
+    name: "契维语（阿坎语）",
+    value: "ak",
+  },
+  {
+    name: "乌克兰语",
+    value: "uk",
+  },
+  {
+    name: "乌尔都语",
+    value: "ur",
+  },
+  {
+    name: "维吾尔语",
+    value: "ug",
+  },
+  {
+    name: "乌兹别克语",
+    value: "uz",
+  },
+  {
+    name: "越南语",
+    value: "vi",
+  },
+  {
+    name: "威尔士语",
+    value: "cy",
+  },
+  {
+    name: "班图语",
+    value: "xh",
+  },
+  {
+    name: "意第绪语",
+    value: "yi",
+  },
+  {
+    name: "约鲁巴语",
+    value: "yo",
+  },
+  {
+    name: "祖鲁语",
+    value: "zu",
+  },
+];
